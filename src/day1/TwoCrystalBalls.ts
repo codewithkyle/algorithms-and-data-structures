d522fbd8d47a34e0d6ef620e13866660788738d4
Kyle Andrews <codingwithkyle@gmail.com>
2023-03-14 08:49:26 -0400

    📝 Added Two Crystal Balls notes

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,16 +1,27 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    // Calc sqrt of N
     const jmpAmount = Math.floor(Math.sqrt(breaks.length));
+
+    // Start at sqrt of N
     let i = jmpAmount;
+
+    // Keep jumping until we find true OR we run out of things to check
     for (; i < breaks.length; i += jmpAmount){
         if (breaks[i]) {
             break;
         }
     }
+
+    // Jump back to last check pos
     i -= jmpAmount;
+
+    // Walk the array looking for our first true value
     for (let j = 0; j < jmpAmount && i < breaks.length; ++j, ++i){
         if (breaks[i]){
             return i;
         }
     }
+
+    // Not found fallback
     return -1;
 }
